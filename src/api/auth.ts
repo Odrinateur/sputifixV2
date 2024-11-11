923c8fbaaf4301af404cdb5f366f04bd60ebae29
Odrinateur <julescbt@gmail.com>
2024-11-11 13:38:36 +0100

    auth spotify

---
+++ src/api/auth.ts
@@ -0,0 +1,33 @@
+export async function GetTokenByCode(code: string) {
+    const clientId = import.meta.env.VITE_CLIENT_ID;
+    const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
+    const redirectUri = import.meta.env.VITE_CALLBACK_URL || '';
+
+    const auth = btoa(`${clientId}:${clientSecret}`)
+
+    const body = new URLSearchParams({
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: redirectUri,
+    });
+
+    try {
+        const response = await fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': `Basic ${auth}`,
+            },
+            body: body.toString(),
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(`Error: ${errorData.error}, Description: ${errorData.error_description}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error('Failed to fetch token:', error);
+    }
+}
