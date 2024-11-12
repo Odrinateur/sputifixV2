dbffb39dd11e21b1c1b73fd15302af9d1a056068
Odrinateur <julescbt@gmail.com>
2024-11-12 11:57:45 +0100

    refresh token

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -31,3 +31,35 @@ export async function GetTokenByCode(code: string) {
         console.error('Failed to fetch token:', error);
     }
 }
+
+export async function RefreshToken(refreshToken: string) {
+    const clientId = import.meta.env.VITE_CLIENT_ID;
+    const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
+
+    const auth = btoa(`${clientId}:${clientSecret}`)
+
+    const body = new URLSearchParams({
+        grant_type: 'refresh_token',
+        refresh_token: refreshToken,
+    });
+
+    try {
+        const response = await fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': `Basic ${auth}`,
+            },
+            body: body.toString(),
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(`Error: ${errorData.error}, Description: ${errorData.error_description}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error('Failed to refresh token:', error);
+    }
+}
