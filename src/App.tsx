4ad4f7fb41c649f3b9d8b0090517d0a0e4096de5
Odrinateur <julescbt@gmail.com>
2024-11-12 19:18:05 +0100

    basic back added

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,23 @@ import Login from '@/pages/Login';
 import Settings from '@/pages/Settings';
 import Callback from "@/pages/Callback.tsx";
 import {AuthProvider} from "@/context/AuthContext.tsx";
+import {StorageProvider} from "@/context/StorageContext.tsx";
 
 
 export default function App() {
     return (
         <AuthProvider>
-            <ThemeProvider>
-                <Router>
-                    <Routes>
-                        <Route path="/" element={secureLocalStorage.getItem('logged_in') === null ? <Login/> : <Home/>}/>
-                        <Route path="/callback" element={<Callback />} />
-                        <Route path="/settings" element={<Settings/>}/>
-                    </Routes>
-                </Router>
-            </ThemeProvider>
+            <StorageProvider>
+                <ThemeProvider>
+                    <Router>
+                        <Routes>
+                            <Route path="/" element={secureLocalStorage.getItem('logged_in') === null ? <Login/> : <Home/>}/>
+                            <Route path="/callback" element={<Callback />} />
+                            <Route path="/settings" element={<Settings/>}/>
+                        </Routes>
+                    </Router>
+                </ThemeProvider>
+            </StorageProvider>
         </AuthProvider>
     );
 }
