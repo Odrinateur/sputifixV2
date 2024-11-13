b20d77b87e00a9aa7d97ed7366d802ef48ff7cb4
Odrinateur <julescbt@gmail.com>
2024-11-13 16:42:11 +0100

    classes

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
+import NavBar from "@/components/NavBar.tsx";
 
 interface MainContainerProps {
     children: React.ReactNode;
-    justifyCenter?: boolean;
+    withNav?: boolean;
     className?: string;
 }
 
-function MainContainer({ children, justifyCenter, className }: MainContainerProps) {
+function MainContainer({children, withNav, className}: MainContainerProps) {
     return (
-        <main className={`flex flex-col p-2 items-center ${justifyCenter ? 'justify-center' : 'justify-start'} h-full ${className}`}>
+        <main
+            className={`${className} flex p-10 w-full h-full ${withNav ? 'flex-row justify-start items-start overflow-y-auto' : 'flex-col justify-start items-center'}`}>
             {children}
         </main>
     );
 }
 
-function MainContainerWithNav({ children, justifyCenter, className }: MainContainerProps) {
+function MainContainerWithNav({children, className}: MainContainerProps) {
     return (
-        <MainContainer className={className} justifyCenter={justifyCenter}>
-            <nav></nav>
-            {children}
+        <MainContainer className={className + " gap-5"} withNav>
+            <div className={"w-4/5 h-full overflow-y-auto"}>
+                {children}
+            </div>
+            <NavBar/>
         </MainContainer>
     );
 }
 
-export { MainContainer, MainContainerWithNav };
+export {MainContainer, MainContainerWithNav};
