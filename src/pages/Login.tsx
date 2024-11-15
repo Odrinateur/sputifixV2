f3d09ec121aed3f9935ab9f74762a7e747907d8a
Odrinateur <julescbt@gmail.com>
2024-11-15 09:27:43 +0100

    justify center container changed

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button"
+import {Button} from "@/components/ui/button"
 import {Link} from "react-router-dom";
 import {MainContainer} from "@/components/MainContainer.tsx";
 import {H1} from "@/components/ui/typography.tsx";
@@ -9,7 +9,7 @@ export default function Login() {
     const url = `https://accounts.spotify.com/fr/authorize?response_type=code&client_id=${clientId}&scope=ugc-image-upload%20user-read-playback-state%20user-modify-playback-state%20user-read-currently-playing%20user-read-email%20user-read-private%20playlist-read-collaborative%20playlist-modify-public%20playlist-read-private%20playlist-modify-private%20user-library-modify%20user-library-read%20user-top-read%20user-read-playback-position%20user-read-recently-played%20user-follow-read%20user-follow-modify&redirect_uri=${callbackUrl}`
 
     return (
-        <MainContainer className={"gap-10"} justifyCenter>
+        <MainContainer className={"gap-10 justify-center"}>
             <H1>Login</H1>
             <Button asChild>
                 <Link to={url}>Login with Spotify</Link>
