6b9d5b7ba7d62fc781781c436ab3f96d815b4359
Odrinateur <julescbt@gmail.com>
2024-11-20 14:33:21 +0100

    responsive

--- src/components/user-card.tsx
+++ src/components/user-card.tsx
@@ -31,7 +31,7 @@ export function UserCard() {
                 ) : (
                     <Skeleton className={'w-2/5 h-full'} />
                 )}
-                <div className={'w-3/5 h-full p-2 flex flex-col items-start gap-1'}>
+                <div className={'w-3/5 h-full p-2 flex flex-col items-start gap-4'}>
                     {user ? (
                         <>
                             <H1 className={'flex justify-center items-center gap-4'}>
