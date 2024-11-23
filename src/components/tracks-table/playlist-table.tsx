8ea995e911b988d6cbe45c3989cdbc5b4c572114
Odrinateur <julescbt@gmail.com>
2024-11-23 20:34:53 +0100

    get playlist in parent to not duplicate api call

--- src/components/tracks-table/playlist-table.tsx
+++ src/components/tracks-table/playlist-table.tsx
@@ -1,19 +1,7 @@
-import { useStorage } from '@/context/StorageContext';
 import { Track } from '@spotify/web-api-ts-sdk';
-import { useEffect, useState } from 'react';
 import { TracksTable } from './tracks-table';
 import { RefreshPlaylistButton } from '../refresh';
 
-export function PlaylistTable({ id }: { id: string }) {
-    const { getPlaylist } = useStorage();
-    const [tracks, setTracks] = useState<Track[] | null>(null);
-
-    useEffect(() => {
-        (async () => {
-            if (tracks) return;
-            await getPlaylist(id).then(([, playlistTracks]) => setTracks(playlistTracks));
-        })();
-    }, [getPlaylist, tracks, id]);
-
+export function PlaylistTable({ tracks, id }: { tracks: Track[] | null; id: string }) {
     return <TracksTable tracks={tracks} RefreshButton={RefreshPlaylistButton} id={id} />;
 }
--- src/pages/Playlist.tsx
+++ src/pages/Playlist.tsx
@@ -2,10 +2,30 @@ import { PlaylistCard } from '@/components/playlist-card';
 import { PlaylistTable } from '@/components/tracks-table/playlist-table';
 import { MainContainerWithNav } from '@/components/ui/main-container';
 import { useNavigate, useParams } from 'react-router-dom';
+import { useStorage } from '@/context/StorageContext';
+import { useEffect, useState } from 'react';
+import { SimplifiedPlaylist, Track } from '@spotify/web-api-ts-sdk';
 
 export function PlaylistPage() {
     const { id } = useParams<{ id: string }>();
     const navigate = useNavigate();
+    const { getPlaylist } = useStorage();
+    const [playlist, setPlaylist] = useState<SimplifiedPlaylist | null>(null);
+    const [tracks, setTracks] = useState<Track[] | null>(null);
+
+    useEffect(() => {
+        if (!id) return;
+
+        (async () => {
+            const [playlistData, playlistTracks] = await getPlaylist(id);
+            if (!playlistData && !playlistTracks) {
+                navigate('/playlists');
+                return;
+            }
+            setPlaylist(playlistData);
+            setTracks(playlistTracks);
+        })();
+    }, [id, getPlaylist, navigate]);
 
     if (!id) {
         navigate('/playlists');
@@ -14,8 +34,8 @@ export function PlaylistPage() {
 
     return (
         <MainContainerWithNav>
-            <PlaylistCard id={id} />
-            <PlaylistTable id={id} />
+            <PlaylistCard playlist={playlist} />
+            <PlaylistTable tracks={tracks} id={id} />
         </MainContainerWithNav>
     );
 }
